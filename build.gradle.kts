8a646b80561b79c40d9365720ae2310b0a0fdafc
gopinathgg55-dot <gopinath.gg55@gmail.com>
2026-09-19 00:39:47 +0530

    Refactor compileOptions and kotlinOptions configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-android {
-    // ... மற்ற அமைப்புகள் ...
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+kotlinOptions {
+    jvmTarget = "17"
 }
